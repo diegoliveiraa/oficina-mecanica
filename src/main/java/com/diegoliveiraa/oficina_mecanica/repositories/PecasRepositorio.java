6cec54e63435d53836c151de32d9b461d67caaed
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-03 23:47:12 -0300

    feat: method adjusts

--- src/main/java/com/diegoliveiraa/oficina_mecanica/repositories/PecasRepositorio.java
+++ src/main/java/com/diegoliveiraa/oficina_mecanica/repositories/PecasRepositorio.java
@@ -3,7 +3,9 @@ package com.diegoliveiraa.oficina_mecanica.repositories;
 import com.diegoliveiraa.oficina_mecanica.entity.Pecas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PecasRepositorio extends JpaRepository<Pecas, UUID> {
+    Optional<Pecas> buscarPecaPorId(UUID id);
 }
